52c13cd710d6d11903891bb2c2c0179b15f22ec6
Andrey Arkhipov <arhipov11@yandex.ru>
2022-08-11 21:47:56 +0300

    create Container component

---
+++ src/components/container.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+  width: 100%;
+  max-width: 1920px;
+  aspect-ratio: 16/9;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Container;
