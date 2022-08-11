388cf9cf7e1efe0e403f770ea775aa27ef0d6e68
Andrey Arkhipov <arhipov11@yandex.ru>
2022-08-11 21:49:53 +0300

    added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
+
 import "./index.css";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+ body{ margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
