a4ab623e7dd4b267727a7595d4a6ef2ac03db7f1
Andrey Arkhipov <arhipov11@yandex.ru>
2022-08-11 21:49:21 +0300

    added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import {useState} from "react";
+import {Routes, Route} from "react-router-dom";
+
+import AuthPage from "./pages/authorization";
+import {IUser} from "./utils/types";
+
 function App() {
+  const [user, setUser] = useState<IUser>({login: ""});
+
   return (
-    <div className="App">
-     <p>Test</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<AuthPage setUser={setUser} />} />
+      <Route path="/profile" element={<p>Profile</p>} />
+    </Routes>
   );
 }
 
